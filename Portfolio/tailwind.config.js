0a454cfe568972530b536af2d3d29f325b1c882c
John Rey Fernandez <john.rey.d.fernandez08@gmail.com>
2025-01-01 00:57:47 +0800

    fix:import syntax

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
@@ -58,5 +59,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), tailwindScrollbar],
+  plugins: [tailwindcssAnimate, tailwindScrollbar],
 };
